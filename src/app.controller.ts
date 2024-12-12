378f95fbad5cca2f90fc6d0f6bec1c8a263a3225
3TERRRNITY <3terrrnity@gmail.com>
2024-12-12 19:57:24 +0300

    add prisma and docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { DbService } from './db/db.service';
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbservice: DbService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto,
+  })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.dbservice.user.findMany({});
+    console.log(users);
+    return { message: this.appService.getHello() };
   }
 }
